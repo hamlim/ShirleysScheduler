7570a29c5e40415fae5fa74b86a39630a81fadc5
Albert Armea <albert@albertarmea.com>
2014-03-17 15:59:52 -0400

    Add a simple Vagrant configuration

---
+++ bootstrap.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+PACKAGES='python python-cherrypy python-sqlalchemy'
+MYSQL_PASSWORD='root'
+
+apt-get update
+
+echo "Installing MySQL"
+debconf-set-selections <<< "mysql-server mysql-server/root_password password $MYSQL_PASSWORD"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $MYSQL_PASSWORD"
+apt-get install -y mysql-server
+
+echo "Installing $PACKAGES"
+apt-get install -y $PACKAGES
