839ee27814223edd9c29cb43f756794b25f3a7fb
Shirley's Scheduler <shirleys-scheduler@albertarmea.com>
2014-04-17 21:24:41 +0000

    Use a real config file

--- api/main.py
+++ api/main.py
@@ -8,15 +8,4 @@ class RootServer(object):
     return "it works!"
 
 if __name__ == '__main__':
-  serverConfig = {
-    'server.socket_host': '0.0.0.0',
-    'server.socket_port': 443,
-
-    'server.ssl_module': 'pyopenssl',
-    'server.ssl_certificate': 'certs/ssl.crt',
-    'server.ssl_private_key': 'certs/private.key',
-    'server.ssl_certificate_chain': 'certs/ca.pem'
-  }
-
-  cherrypy.config.update(serverConfig)
-  cherrypy.quickstart(RootServer())
+  cherrypy.quickstart(RootServer(), config="app.config")
