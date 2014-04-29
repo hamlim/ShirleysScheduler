302f1618a5efde77f9c38a93712a47c7b57ba5ea
hamlim <hamlim@rpi.edu>
2014-04-29 00:01:18 -0400

    started settings.js page

---
+++ frontend/js/settings.js
@@ -0,0 +1,15 @@
+$(document).ready( function(){
+	//we need to pull in the data from the input fields and then on a button/enter ajax the data to the server
+	/* 
+		ID's we need to watch:
+			#alias-email
+				user enters an alias email 
+			#nickname
+				user enters a username/name
+			#phone-number
+				user enters a phone Number
+				
+			All of these values are additional things that can be stored in with the Person section of the api/me return values
+	*/
+	
+});
