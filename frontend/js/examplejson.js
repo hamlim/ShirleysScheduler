07dd631ce7ea2e89a014be33481c8d0dbc514d25
hamlim <hamlim@rpi.edu>
2014-04-29 14:31:42 -0400

    Finished examplejson.js file
    Need to pull it into the local storage

--- frontend/js/examplejson.js
+++ frontend/js/examplejson.js
@@ -1,6 +1,6 @@
 //This file will have an example API/me return JS Object "JSON"
 var apime = {
-    Today: {
+    "Today": {
         {
             "meetingname" : "Meeting with Adrian",
             "groupname" : "Friends",
@@ -10,104 +10,103 @@ var apime = {
 			"url" : ""
         }
     },
-    Tomorrow: {
+    "Tomorrow": {
         {
             "meetingname": "Team Meeting",
             "groupname": "Schedulr Team",
             "location": "McNeil Room, Union",
             "timebegin": 1399042800,
             "timeend": 1399050000,
-			"url" : ""
+			"url": ""
         },
         {    
-            "meetingname" : "Chat with V. Kudinov",
-            "groupname" : "ACME inc",
-            "location" : "Skype conference",
-            "timebegin" : 1398934800,
-            "timeend" : 1398938400,
-						url : ""
+            "meetingname": "Chat with V. Kudinov",
+            "groupname": "ACME inc",
+            "location": "Skype conference",
+            "timebegin": 1398934800,
+            "timeend": 1398938400,
+			"url": ""
         }
     },
-    Yesterday: {
+    "Yesterday": {
         {
             "meetingname": "Team Meeting",
             "groupname": "SS inc",
             "location": "3rd floor, Union",
             "timebegin": 1398870000,
             "timeend": 1398877200,
-			"url" = ""
+			"url": ""
         }
     },
-    Invites: {
+    "Invites": {
         {
-            "eventid": 1234,
+            "eventid": 12345678,
             "meetingname": "Team Meeting",
             "owner": "Matt Hamlin"
         },
         {
-            "eventid": "eventid",
-            "meetingname": "name",
-            "owner": "Name of event creator"
+            "eventid": 12345679,
+            "meetingname": "Meet and Greet",
+            "owner": "Steve"
         }
     },
-    Groups: {
+    "Groups": {
         { 
-            "groupname": "blah",
+            "groupname": "SS inc",
             "groupid": 12345
         },
 		{
 		  	"groupname": "Friends",
 			"groupid": 12346
+		},
+		{
+		  	"groupname": "ACME inc",
+			"groupid": 12347
 		}
     },
-    Calendar: { 
+    "Calendar": { 
         {
-            meetingname : "Team Meeting",
-            groupname : "SS inc",
-            location : "3rd floor, Union",
-            timebegin : 1398870000,
-            timeend : 1398877200,
-            url : ""
+            "meetingname": "Team Meeting",
+            "groupname": "SS inc",
+            "location": "3rd floor, Union",
+            "timebegin": 1398870000,
+            "timeend": 1398877200,
+            "url": ""
         },
         {
-            meetingname : "Meeting with Adrian",
-            groupname : "Friends",
-            location : "Times Square",
-            timebegin : 1398967200,
-            timeend : 1398970800,
-            url : ""
+            "meetingname": "Meeting with Adrian",
+            "groupname": "Friends",
+            "location": "Times Square",
+            "timebegin": 1398967200,
+            "timeend": 1398970800,
+            "url": ""
         },
         {
-            meetingname : "Team Meeting",
-            groupname : "Schedulr Team",
-            location: "McNeil Room, Union",
-            timebegin: 1399042800,
-            timeend: 1399050000,
-            url : ""
+            "meetingname": "Team Meeting",
+            "groupname": "Schedulr Team",
+            "location": "McNeil Room, Union",
+            "timebegin": 1399042800,
+            "timeend": 1399050000,
+            "url": ""
         },
 		{
-			meetingname			
+			"meetingname": "Chat with V. Kudinov",
+			"groupname": "ACME inc",
+			"location": "Skype conference",
+			"timebegin": 1398934800,
+			"timeend": 1398938400,
+			"url": ""
 		}
-    }
-    Person : {
-        name : "Joe smith",
-        email : "gmail",
-        aliasemail : "outlook",
-        username : "usernamehere",
-				phonenumber : "Phone number here!",
-        groups : {
-            {
-                groupname : "name",
-                groupid : "id"
-            },
-            {
-                groupname : "name",
-                groupid : "id"
-            },
-        },
-        datejoined : int,
-        profileimg : "link to profile image",
-        description : "description text",
-        googlelink : "link to google+ profile"
+    },
+    "Person": {
+        "name": "Jaime Lannister",
+        "email": "jlannister@aol.com",
+        "aliasemail": "kingslayer434@outlook.com",
+        "username": "k1ng5layr",
+		"phonenumber": "518-555-0160",
+        "datejoined": 1398517200,
+        "profileimg": "http://i.imgur.com/sx67gNb.jpg",
+        "description": "Knight from the house of Lannister, moved to Albany for an amazing job.",
+        "googlelink": ""
     }
 }
