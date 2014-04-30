080baa80cea7ba60c137d4f989eced9488bf6273
hamlim <hamlim@rpi.edu>
2014-04-30 16:19:54 -0400

    removed buggy render thingy

--- frontend/js/dashboard.js
+++ frontend/js/dashboard.js
@@ -50,7 +50,6 @@ $(document).ready(function(){
 	var apime = JSON.parse(obj);
 	console.log(apime); //apime stores all the data from the database
 	//---------------------------------------------------------------------------------------------------------------------------
-	$('#pcalendar').fullCalendar('render');
 	var s = new Date();
 	s.setDate(s.getDate() - 2);
 	//s is the start date for the event generating function
