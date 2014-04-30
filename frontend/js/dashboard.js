863e96661741da1995fdb8a0cb90ca2243434d99
hamlim <hamlim@rpi.edu>
2014-04-30 18:14:44 -0400

    Fixed enumerating events
    removed footer because its silly
    More html changes to come along the way

--- frontend/js/dashboard.js
+++ frontend/js/dashboard.js
@@ -165,8 +165,8 @@ $(document).ready(function(){
 	var invites = $("#user-meeting-invite");
 	content = "";
 	for (var k = 0; k< apime["Invites"].length; k++){
-		var eventlink = "<a href='https://shirleys-scheduler.com/group.html' title='Event Information'>" + apime["Invites"][i].meetingname + "</a>";
-		var invite = "<h4>" + apime["Invites"][i].owner + " invited you! </h4>";
+		var eventlink = "<a href='https://shirleys-scheduler.com/group.html' title='Event Information'>" + apime["Invites"][k].meetingname + "</a>";
+		var invite = "<h4>" + apime["Invites"][k].owner + " invited you! </h4>";
 		content += libegin + invite + eventlink + liend;
 	};
 	if (content == ""){
