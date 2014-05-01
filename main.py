6b799609b14d044274f1d8bdc2437f83ee9e88e6
Shirley's Scheduler <shirleys-scheduler@albertarmea.com>
2014-05-01 09:50:57 +0000

    Enable external API use

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ from auth import AuthRoot
 from api import ApiRoot
 from schemas import ClassBase
 
+def CORS():
+  cherrypy.response.headers["Access-Control-Allow-Origin"] = "*"
+
 class Root(object):
   def __init__(self, settings):
     # TODO: Add a way to change the database username and password
@@ -21,9 +24,11 @@ class Root(object):
     self.api = ApiRoot(settings, sessionFactory)
 
 if __name__ == '__main__':
+  cherrypy.tools.CORS = cherrypy.Tool('before_finalize', CORS)
   configParser = ConfigParser.ConfigParser()
   configParser.read("api.config")
   root = Root(configParser)
   cherrypy.tree.mount(root.api, '/api', config={'/': {
-    'request.dispatch': cherrypy.dispatch.MethodDispatcher()}})
+    'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+    'tools.CORS.on': True}})
   cherrypy.quickstart(root, config="app.config")
