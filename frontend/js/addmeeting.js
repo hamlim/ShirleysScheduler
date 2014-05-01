23ff4cd5eb055d4417634d77e0abe234a7e35b7f
Daniel Gilligan <daniel.j.gilligan@gmail.com>
2014-05-01 06:50:14 -0400

    something

--- frontend/js/addmeeting.js
+++ frontend/js/addmeeting.js
@@ -2,8 +2,8 @@
     $(document).ready(function() {	
 
        var data = localStorage.getItem("apime");
-			 var me = JSON.parse(data);
-			 if (me == null || me == 'undefined'){
+			 var apime = JSON.parse(data);
+			 if (apime == null || apime == 'undefined'){
 				 window.location.replace('index.html');
 			 } else {
 				 //Navigation Menu Slider
@@ -39,15 +39,17 @@
 					$("#end").datetimepicker();
 
 					// Can't repeat until date & time selected first
+          /*
 					if (Date.parse($("#start.input-group.date input[type=text]").val()) <= 0 || Date.parse($("#end.input-group.date input[type=text]").val()) <= 0) {
 						document.getElementById("meeting-repeat").disabled=true;
-					}
+					}*/
 
 					// Undisable checkbox if both date boxes are filled
 					// Need to fix bug - only one date filled and checkbox active again
+          /*
 					$("#start.input-group.date input[type=text], #end.input-group.date input[type=text]").change(function() {
 						document.getElementById("meeting-repeat").disabled=false;
-					});
+					});*/
 
 					/*
 					var startFilled = $("#start .input-group.date input[type=text]").change(function() {
@@ -69,6 +71,132 @@
 						// Do your thing here!
 					});
 					*/
+
+          function eventarr(object){
+      //note object is really only going to be apime
+      //we need to isolate the events in the object
+      // Note meeting location is a custom additional value
+      var pubcalarr = [];
+      for (var i=0; i<object["Calendar"].length; i++){
+        //object["Calendar"][i] is an event
+        //we want to isolate those events with and w/o a url
+
+        if(object["Calendar"][i]["url"] == "" || object["Calendar"][i]["url"] == null){
+
+          //no url to worry about:
+
+          var start = new Date(object["Calendar"][i]["timebegin"]*1000);
+          var end = new Date(object["Calendar"][i]["timeend"]*1000)
+          var name = object["Calendar"][i]["meetingname"];
+          var loc = object["Calendar"][i]["location"];
+          pubcalarr.push({
+            title: name,
+            start: start,
+            end: end,
+            location: loc,
+            gname: object["Calendar"][i]["groupname"]
+          });
+        } else {
+          //there is a url
+          var start = new Date(object["Calendar"][i]["timebegin"]*1000);
+          var end = new Date(object["Calendar"][i]["timeend"]*1000);
+          var url = object["Calendar"][i]["url"];
+          var loc = object["Calendar"][i]["location"];
+          pubcalarr.push({
+            title: name,
+            start: start,
+            end: end,
+            location: loc,
+            gname: object["Calendar"][i]["groupname"],
+            url: url
+          });
+        };
+      };
+      return pubcalarr;
+    };
+
+
+    $("#pcalendar").fullCalendar({
+      //here we will:
+      // set the view to a month
+      // import the events
+      // enable onclick of events
+      defaultView: 'month',
+      header: {
+        left: 'prev, next, today',
+        center: 'title',
+        right: ''
+      },
+      editable: false,
+      events: eventarr(apime),
+      // ADD EVENTSOURCES
+      eventClick: function(calEvent, jsEvent, view){
+        //calEvent.datum = datum value
+        //we make a modal first
+
+        var day = "";
+        function blah() {
+          //generate a day name based on getUTCdate
+          var val = calEvent.start.getUTCDay();
+          if(val == 1){
+            day = "Monday";
+          } else if (val == 2){
+            day = "Tuesday";
+          } else if( val == 3){
+            day = "Wednesday";
+          } else if (val == 4){
+            day = "Thursday";
+          } else if (val == 5){
+            day = "Friday";
+          } else if (val == 6){
+            day = "Saturday";
+          } else {
+            day = "Sunday";
+          }
+        };
+        blah();
+
+        if(typeof calEvent.url != 'undefined' && calEvent.url != "" && calEvent.url != null){
+
+          if(calEvent.loc == null || calEvent.loc == ""){
+            $("#modal-title").text("Meeting Name: " + calEvent.title);
+            $("#group-name").text("Group Name: " + calEvent.gname);
+            $("#location").text("No location specified.");
+            $("#url").append("<a href=" + calEvent.url + ">Link</a>");
+            $("#meeting-date").text("Meeting Date: " + day);
+            $("#meeting-time").text("Meeting Time: " + calEvent.start.getUTCHours());
+            $("#popout-modal").modal('show');
+          } else {
+            $("#modal-title").text("Meeting Name: " + calEvent.title);
+            $("#group-name").text("Group Name: " + calEvent.gname);
+            $("#location").text("Location: " + calEvent.loc);
+            $("#url").append("<a href=" + calEvent.url + ">Link</a>");
+            $("#meeting-date").text("Meeting Date: " + day);
+            $("#meeting-time").text("Meeting Time: " + calEvent.start.getUTCHours());
+            $("#popout-modal").modal('show');
+          }
+        } else {
+          if(calEvent.loc == null || calEvent.loc == ""){
+
+            $("#modal-title").text("Meeting Name: " + calEvent.title);
+            $("#group-name").text("Group Name: " + calEvent.gname);
+            $("#location").text("No location specified.");
+            $("#meeting-date").text("Meeting Date: " + day);
+            $("#meeting-time").text("Meeting Time: " + calEvent.start.getUTCHours());
+            $("#popout-modal").modal('show');
+          } else {
+            $("#modal-title").text("Meeting Name: " + calEvent.title);
+            $("#group-name").text("Group Name: " + calEvent.gname);
+            $("#location").text("Location: " + calEvent.loc);
+            $("#meeting-date").text("Meeting Date: " + day);
+            $("#meeting-time").text("Meeting Time: " + calEvent.start.getUTCHours());
+            $("#popout-modal").modal('show');
+          }
+          console.log("Event " + calEvent);
+        };
+
+      }
+    });
 
 					// Pick group for meeting
 					for (var i = 0; i < apime["Groups"].length; ++i) {
--- frontend/js/calendar.js
+++ frontend/js/calendar.js
@@ -159,5 +159,5 @@ $(document).ready(function(){
 
 			}
 		});
-	};
+	}
 });
