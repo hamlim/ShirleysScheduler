b5075a40d6b5dd78cdf112d0e48a21944567ea1b
hamlim <hamlim@rpi.edu>
2014-05-01 06:00:45 -0400

    fixes???

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -23,12 +23,13 @@ $(document).ready(function(){
 		$.ajax({
 			url: "https://shirleys-scheduler.com/auth/login",
 			type: "GET",
+			dataType: "JSON",
 			success: function(toke){
-				console.log("it worked");
 				// now we have an invalid token
 				// token is stored in data
 				localStorage.setItem("token", toke["token"]);
-				window.location.replace("https://shirleys-scheduler.com/auth/login_validate?token="+toke['token']);
+				console.log(toke);
+				window.location.replace("https://shirleys-scheduler.com/auth/login_validate?token="+toke["token"]);
 			},
 			error: function(xhr, status, error){
 				console.log("AJAX error: " + error);
