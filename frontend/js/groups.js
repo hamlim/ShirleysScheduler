74293e3d4e080e82205070bece577eb2cf4db38e
Adriana Morales <adriana.a.morales@live.com>
2014-05-01 14:21:15 -0400

    Updating UI

--- frontend/js/groups.js
+++ frontend/js/groups.js
@@ -41,8 +41,8 @@ $(document).ready(function(){
 			$("#list-users").append('<h5 class="list-group-item">'+groups["Users"][i].email+'</h5>');
 		}
 		for (var i = 0; i < groups["Events"].length; i++) {
-			$("#group-events").append("<div><h3 class='list-group-item'>"+groups["Events"][i].meetingname+"</h3><blockquote><em>Location:</em> " +groups["Events"][i].location+"<br/>" + 
-			"<em>Start: </em>"+new Date(groups["Events"][i].timebegin)+"<br/><em>End: </em>"+new Date(groups["Events"][i].timeend) +"</blockquote></div>"
+			$("#group-events").append("<div class='panel-heading'><h3 class='list-group-item'>"+groups["Events"][i].meetingname+"</h3></div><div class='todo'><em>Location:</em> " +groups["Events"][i].location+"<br/>" + 
+			"<em>Start: </em>"+new Date(groups["Events"][i].timebegin)+"<br/><em>End: </em>"+new Date(groups["Events"][i].timeend) +"</div>"
 			);
 
 		}
