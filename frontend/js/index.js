a112031481b2a3468f3a319eef89704a7107388d
hamlim <hamlim@rpi.edu>
2014-05-07 19:40:13 -0400

    Commenting things

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -4,16 +4,9 @@ $(document).ready(function(){
 	//  make a call to the api and retrieve the data from /api/me
 	//  store the data in local storage
 	//-------------------------------------
-	//for TESTING only:
-//	var data = apime;
 	localStorage.clear();
-	//localStorage.setItem("groups", JSON.stringify(group));
-//	localStorage.setItem("apime", JSON.stringify(data));
-//	// Now all pages should get the data added to local storage
-//	//test local storage
-//	var obj = localStorage.getItem("apime");
-//	var dat = JSON.parse(obj);
-//	console.log(dat["Person"]["name"]);
+	// We clear the local storage so that the user will have to login again
+	// This is simply so we don't worry about non-up-to-date information
 
 	//-------------------------------------
 	var login = $("#google-login");
@@ -35,7 +28,8 @@ $(document).ready(function(){
 			window.location.replace("https://shirleys-scheduler.com/auth/login_validate?token="+toke["token"]); // now we pass that token on to be validated by the server
 		});
 		drequest.fail(function(xhr, status, error){
-				console.log("AJAX error: " + error);
+			console.log("AJAX error: " + error);
+			console.log("status: " + status);
 
 		});
 	});
