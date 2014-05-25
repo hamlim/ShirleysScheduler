8537a29c3945cbfa9e0a2c8ca17e9d6a662b6f0c
hamlim <hamlim@rpi.edu>
2014-05-25 11:19:15 -0700

    Some basic JS edits

--- frontend/js/meeting.js
+++ frontend/js/meeting.js
@@ -6,6 +6,19 @@ $(document).ready(function(){
 		window.location.replace( 'index.html' );
 	} else {
 		// Main code belongs within here..
+		//we want to create pointers to the things we will want to change
+		var titleelem = $('title');
+		var meetingnameelem = $('#meeting-name');
+		var meetingtimeelem = $('#meeting-time');
+		var peoplegoingelem = $('#p-going');
+		var peoplenotgoingelem = $('#not-p-going');
+		var meetinglocationelem = $('#meeting-location');
+		var groupnameelem = $("#group-name");
+		var documentlistelem = $("#document-list");
+		// the three following selctors are used for the three different API's
+		var trelloTreeHead = $("#todo-list");
+		var evernoteTreeHead = $("#meeting-notes");
+		var googleDriveTreeHead = $("#documents");
 		
 	}
-})
+});
